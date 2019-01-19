86fc96af889f0e681a6aa0c4e637f865f7a4a9fc
Won Pyoung Choi <cwp10@msn.com>
2019-01-20 02:01:00 +0900

    에셋번들 메니저 제작

--- Assets/Scripts/AssetBundleManager.cs
+++ Assets/Scripts/AssetBundleManager.cs
@@ -1,157 +1,68 @@
-﻿using System.IO;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Networking;
+using Network;
 
-namespace Network
+public class AssetBundleManager : UnitySingleton<AssetBundleManager>
 {
-    public class AssetBundleManager : MonoBehaviour
+    private AssetBundleClient _assetBundleClient = null;
+
+    public float Progress
     {
-        private enum State
+        get
         {
-            Ready = 0,
-            Busy,
+            return _assetBundleClient.Progress;
         }
+    }
 
-        private class RequestItem
+    public int TotalDownloadCount
+    {
+        get
         {
-            public string url = string.Empty;
-            public UnityAction<string> action = null;
-
-            public RequestItem(string url, UnityAction<string> action)
-            {
-                this.url = url;
-                this.action = action;
-            }
+            return _assetBundleClient.TotalDownloadCount;
         }
+    }
 
-        private Dictionary<string, string> _assetObjectNameDic = new Dictionary<string, string>();
-        private Queue<RequestItem> _requestQueue = new Queue<RequestItem>();
-        private State _state = State.Ready;
-        private AssetDownLoader _assetDownLoader = null;
-        private string DataPath
+    public int DownloadCount
+    {
+        get
         {
-            get
-            {
-                DirectoryInfo directory = new DirectoryInfo(Application.dataPath);
-
-#if UNITY_EDITOR && UNITY_ANDROID
-                return "file://" + directory.Parent.ToString() + "/AssetBundles/android";
-#elif UNITY_EDITOR && UNITY_IOS
-                return "file://" + directory.Parent.ToString() + "/AssetBundles/ios";
-#elif UNITY_EDITOR && UNITY_STANDALONE
-                return "file://" + directory.Parent.ToString() + "/AssetBundles/pc";
-#elif UNITY_ANDROID
-                return Path.Combine(downloadUrl, "android");
-#elif UNITY_IOS
-                return Path.Combine(downloadUrl, "ios");
-#elif UNITY_STANDALONE
-                return Path.Combine(downloadUrl, "pc");
-#endif
-            }
+            return _assetBundleClient.DownloadCount;
         }
+    }
 
-        public bool IsBusy { get { return _state != State.Ready; } }
-        public string downloadUrl = string.Empty;
+    public void Init()
+    {
+        _assetBundleClient = new AssetBundleClient();
+    }
 
-        private void Start()
-        {
-            _assetDownLoader = new AssetDownLoader();
-            this.DownloadedAssetBundleManifest();
-        }
+    public void DownloadAllAssetBundle(string url, UnityAction<string> action)
+    {
+        _assetBundleClient.DownloadedAssetBundleManifest(url, action);
+    }
 
-        private void DownloadedAssetBundleManifest()
-        {
-            string[] folders = DataPath.Split('/');
-            string manifestName = folders[folders.Length - 1];
-            string path = Path.Combine(DataPath, manifestName);
-            StartCoroutine(_assetDownLoader.DownloadAndCache(path, OnDownloadedAssetBundleManifest));
-        }
+    public void DownloadAssetBundle(string url, UnityAction<string> action)
+    {
+        _assetBundleClient.DownloadAssetBundle(url, action);
+    }
 
-        private void NextDownload()
-        {
-            if (_requestQueue.Count <= 0)
-            {
-                return;
-            }
-            if (this.IsBusy)
-            {
-                return;
-            }
-            StartCoroutine("ProcessRequest", _requestQueue.Peek());
-        }
-        private IEnumerator ProcessRequest(RequestItem item)
-        {
-            _state = State.Busy;
-            yield return StartCoroutine(_assetDownLoader.DownloadAndCache(item.url, item.action));
+    public T LoadAsset<T>(string name) where T : Object
+    {
+        return _assetBundleClient.LoadAsset<T>(name);
+    }
 
-            _requestQueue.Dequeue();
-            _state = State.Ready;
-            this.NextDownload();
-        }
+    public void Unload(string url, bool unloadLoadedObjects)
+    {
+        _assetBundleClient.Unload(url, unloadLoadedObjects);
+    }
 
-        private void OnDownloadedAssetBundleManifest(string url)
-        {
-            AssetBundle bundle = _assetDownLoader.GetAssetBundle(url);
-            var manifest = bundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
-            string[] names = manifest.GetAllAssetBundles();
+    public void AllUnload(bool unloadAllLoadedObjects)
+    {
+        _assetBundleClient.AllUnload(unloadAllLoadedObjects);
+    }
 
-            foreach (string name in names)
-            {
-                string path = Path.Combine(DataPath, name);
-                this.DownloadAssetBundle(path, OnDownloadedAssetBundle);
-            }
-        }
-
-        private void OnDownloadedAssetBundle(string url)
-        {
-            AssetBundle bundle = _assetDownLoader.GetAssetBundle(url);
-            string[] names = bundle.GetAllAssetNames();
-
-            foreach (string name in names)
-            {
-                _assetObjectNameDic.Add(name, url);
-            }
-        }
-
-        public float DownloadProgress()
-        {
-            return _assetDownLoader.Progress;
-        }
-
-        public void DownloadAssetBundle(string url, UnityAction<string> action = null)
-        {
-            _requestQueue.Enqueue(new RequestItem(url, action));
-            this.NextDownload();
-        }
-
-        public T LoadAsset<T>(string name) where T : Object
-        {
-            string url;
-
-            if (_assetObjectNameDic.TryGetValue(name, out url))
-            {
-                AssetBundle bundle = _assetDownLoader.GetAssetBundle(url);
-                return bundle.LoadAsset<T>(name);
-            }
-            return null;
-        }
-
-        public void Unload(string url, bool unloadAllLoadedObjects)
-        {
-            _assetDownLoader.Unload(url, unloadAllLoadedObjects);
-        }
-
-        public void AllUnload(bool unloadAllLoadedObjects)
-        {
-            _assetDownLoader.AllUnload(unloadAllLoadedObjects);
-        }
-
-        public void ClearCache()
-        {
-            Caching.ClearCache();
-        }
+    public void ClearCache()
+    {
+        _assetBundleClient.ClearCache();
     }
 }
