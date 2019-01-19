44ba5a8c9ab6027630b53192639572b26008c409
Won Pyoung Choi <cwp10@msn.com>
2019-01-20 01:59:02 +0900

    코루틴 헨들러 추가

---
+++ Assets/Scripts/CoroutineHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineHandler : UnitySingleton<CoroutineHandler>
+{
+    public static Coroutine StartStaticCoroutine(IEnumerator coroutine)
+    {
+        return Instance.StartCoroutine(coroutine);
+    }
+
+    public static Coroutine StopStaticCoroutine(IEnumerator coroutine)
+    {
+        return Instance.StartCoroutine(coroutine);
+    }
+}
